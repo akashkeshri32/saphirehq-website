75f16bd2eaa3030b19d7542581d18d6b9c79c738
akashkeshri32 <contactakashkeshri@gmail.com>
2026-08-19 00:13:02 +0530

    testimonial content in placement statistics page

--- components/sections/placementSuccessStoriesSection/placementSuccessStoryCard.tsx
+++ components/sections/placementSuccessStoriesSection/placementSuccessStoryCard.tsx
@@ -6,8 +6,10 @@ export type PlacementSuccessStory = {
   name: string;
   designation: string;
   testimonial: string;
-  role: string;
-  hikePercentage: string;
+  role?: string;
+  highlight: string;
+  linkedinUrl?: string
+  company : string
 };
 
 export const PlacementSuccessStoryCard = ({
@@ -16,22 +18,24 @@ export const PlacementSuccessStoryCard = ({
   designation,
   testimonial,
   role,
-  hikePercentage,
+  highlight,
+  linkedinUrl,
+  company
 }: PlacementSuccessStory) => {
   return (
     <div className="bg-white border border-border-stroke rounded-xl p-6.5 h-full flex flex-col justify-between">
       <div>
-        <Author avatar={avatar} name={name} designation={designation} />
+        <Author avatar={avatar} name={name} designation={designation} linkedinUrl={linkedinUrl} />
 
-        <p className="text-14 text-black mt-5">{testimonial}</p>
+        <div className="text-14 text-black mt-5 space-y-2" dangerouslySetInnerHTML={{ __html : testimonial}} />
       </div>
 
       <div className="border-t border-border-stroke mt-6 pt-4 flex items-center justify-between">
         <span className="font-jetbrains text-12 font-normal text-green">
-          {role}
+          {company}
         </span>
         <span className="font-jetbrains text-12 font-extrabold text-green">
-          {hikePercentage}
+          {highlight}
         </span>
       </div>
     </div>
